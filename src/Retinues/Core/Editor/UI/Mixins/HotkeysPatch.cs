1867d0adecbbc4e6ed2ef0ad9f72cf6cce6909a8
bolaft <2310791+bolaft@users.noreply.github.com>
2025-09-28 21:37:26 +0200

    path name

--- src/Retinues/Core/Editor/UI/Mixins/HotkeysPatch.cs
+++ src/Retinues/Core/Editor/UI/Mixins/HotkeysPatch.cs
@@ -2,19 +2,19 @@
 using HarmonyLib;
 using TaleWorlds.CampaignSystem.ViewModelCollection.ClanManagement;
 
-public static class ClanTabBlocker
+public static class HotkeyBlocker
 {
-    public static volatile bool BlockTabSwitch = true;
+    public static volatile bool BlockHotkeys = true;
 }
 
 [HarmonyPatch(typeof(ClanManagementVM))]
-static class ClanManagementVM_TabGuards
+static class ClanManagementVM_HotkeyGuards
 {
     [HarmonyPrefix]
     [HarmonyPatch(nameof(ClanManagementVM.SelectPreviousCategory))]
-    static bool Prev_Prefix() => !ClanTabBlocker.BlockTabSwitch;
+    static bool Prev_Prefix() => !HotkeyBlocker.BlockHotkeys;
 
     [HarmonyPrefix]
     [HarmonyPatch(nameof(ClanManagementVM.SelectNextCategory))]
-    static bool Next_Prefix() => !ClanTabBlocker.BlockTabSwitch;
+    static bool Next_Prefix() => !HotkeyBlocker.BlockHotkeys;
 }
