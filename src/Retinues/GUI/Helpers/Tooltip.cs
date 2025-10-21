7a1b105cd63251b4e663da93cc7e87e254f23e41
bolaft <2310791+bolaft@users.noreply.github.com>
2025-10-21 10:13:39 +0200

    cleared unused code from previous item tooltip

--- src/Retinues/GUI/Helpers/Tooltip.cs
+++ src/Retinues/GUI/Helpers/Tooltip.cs
@@ -1,9 +1,6 @@
 using System.Collections.Generic;
-using Retinues.Game.Wrappers;
 using Retinues.Utils;
-using TaleWorlds.Core.ViewModelCollection;
 using TaleWorlds.Core.ViewModelCollection.Information;
-using TaleWorlds.MountAndBlade;
 
 namespace Retinues.GUI.Helpers
 {
@@ -41,53 +38,6 @@ namespace Retinues.GUI.Helpers
                             TooltipProperty.TooltipPropertyFlags.None
                         )
                     );
-                return props;
-            });
-        }
-
-        /// <summary>
-        /// Builds a tooltip for an item, showing tier, class, culture, and statistics.
-        /// </summary>
-        public static BasicTooltipViewModel MakeItemTooltip(WItem item)
-        {
-            if (item == null)
-                return null;
-
-            return new BasicTooltipViewModel(() =>
-            {
-                string titleText;
-
-                if (item.Culture?.Name != null)
-                    titleText = $"T{item.Tier} {item.Class} ({item.Culture.Name})";
-                else
-                    titleText = $"T{item.Tier} {item.Class}";
-
-                var props = new List<TooltipProperty>
-                {
-                    // Title
-                    new(
-                        string.Empty,
-                        titleText,
-                        0,
-                        false,
-                        TooltipProperty.TooltipPropertyFlags.Title
-                    ),
-                };
-
-                // Description
-                foreach (var stat in item.Statistics)
-                {
-                    props.Add(
-                        new TooltipProperty(
-                            string.Empty,
-                            $"{stat.Key}: {stat.Value}",
-                            0,
-                            false,
-                            TooltipProperty.TooltipPropertyFlags.None
-                        )
-                    );
-                }
-
                 return props;
             });
         }
