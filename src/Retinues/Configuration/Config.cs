796352bf7855158c44badb89ae63bde9d417061a
bolaft <2310791+bolaft@users.noreply.github.com>
2025-11-11 12:50:54 +0100

    global editor disabled by default, is opt-in

--- src/Retinues/Configuration/Config.cs
+++ src/Retinues/Configuration/Config.cs
@@ -314,11 +314,11 @@ namespace Retinues.Configuration
                     "mcm_option_global_editor_enabled_hint",
                     "Enables the global troop editor to modify any troop in the game. Disable if you are encountering issues with non-player troops."
                 ),
-            @default: true,
+            @default: false,
             presets: new Dictionary<string, object>
             {
-                [Presets.Freeform] = true,
-                [Presets.Realistic] = true,
+                [Presets.Freeform] = false,
+                [Presets.Realistic] = false,
             },
             requiresRestart: true,
             disabled: ModCompatibility.NoGlobalEditor,
