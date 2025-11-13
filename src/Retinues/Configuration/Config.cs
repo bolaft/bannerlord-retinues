6db9398093a66c46e551669699435b76e0e35cdb
bolaft <2310791+bolaft@users.noreply.github.com>
2025-11-13 18:52:16 +0100

    max 3 upgrades for elite retinues, so doctrine has an effect

--- src/Retinues/Configuration/Config.cs
+++ src/Retinues/Configuration/Config.cs
@@ -383,10 +383,10 @@ namespace Retinues.Configuration
                 ),
             @default: 1,
             minValue: 1,
-            maxValue: 4,
+            maxValue: 3,
             presets: new Dictionary<string, object>
             {
-                [Presets.Freeform] = 4,
+                [Presets.Freeform] = 3,
                 [Presets.Realistic] = 1,
             }
         );
