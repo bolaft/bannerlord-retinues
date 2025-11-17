5cd35b1dde2d5e5fceb3e3b83ef4ec190960e54f
bolaft <2310791+bolaft@users.noreply.github.com>
2025-11-17 09:03:42 +0100

    version change popup

--- src/Retinues/Safety/Version/VersionBehavior.cs
+++ src/Retinues/Safety/Version/VersionBehavior.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using Retinues.GUI.Helpers;
 using Retinues.Utils;
 using TaleWorlds.CampaignSystem;
 
@@ -55,7 +57,42 @@ namespace Retinues.Safety.Version
         /// </summary>
         private void OnGameLoadFinished()
         {
-            Log.Info($"Save File: Retinues {_retinuesVersion ?? "unknown"}");
+            string saveVersionString = _retinuesVersion ?? "unknown";
+            string currentVersionString = ModuleChecker.GetModule("Retinues").Version;
+
+            if (saveVersionString != currentVersionString)
+            {
+                int saveVersionInt = int.Parse(saveVersionString.Split('.').Last());
+                int currentVersionInt = int.Parse(currentVersionString.Split('.').Last());
+
+                Log.Info($"Save File: Retinues {_retinuesVersion ?? "unknown"}");
+
+                if (saveVersionInt + 1 == currentVersionInt)
+                {
+                    Notifications.Popup(
+                        L.T("retinues_update_title", "Retinues Update"),
+                        L.T(
+                                "retinues_version_update_text",
+                                "The Retinues mod version in this save file has been updated to the next version ({CURRENT_VERSION}). Your save data has been automatically migrated to the new version.\n\nAs a safety precaution, check that everything is as it should before overwriting your save.\n\nIf you notice any issues and wish to go back to the previous version, do not save and download the {SAVE_VERSION} file from Nexus Mods."
+                            )
+                            .SetTextVariable("SAVE_VERSION", saveVersionString)
+                            .SetTextVariable("CURRENT_VERSION", currentVersionString)
+                    );
+                    return;
+                }
+                else if (currentVersionInt != saveVersionInt)
+                {
+                    Notifications.Popup(
+                        L.T("retinues_version_change_title", "Retinues Version Change"),
+                        L.T(
+                                "retinues_version_change_text",
+                                "The Retinues mod version in this save file does not match the current mod version ({CURRENT_VERSION}). This may cause issues.\n\nAs a safety precaution, check that everything is as it should before overwriting your save.\n\nIf you notice any issues and wish to go back to the previous version, do not save and download the {SAVE_VERSION} file from Nexus Mods."
+                            )
+                            .SetTextVariable("SAVE_VERSION", saveVersionString)
+                            .SetTextVariable("CURRENT_VERSION", currentVersionString)
+                    );
+                }
+            }
         }
     }
 }
