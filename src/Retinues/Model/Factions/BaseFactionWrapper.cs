c44774f2ae03f7d5b370002c718a666256a4d9c0
bolaft <2310791+bolaft@users.noreply.github.com>
2025-12-10 16:21:30 +0100

    faction cleanup + WSettlement

--- src/Retinues/Model/Factions/BaseFactionWrapper.cs
+++ src/Retinues/Model/Factions/BaseFactionWrapper.cs
@@ -2,15 +2,13 @@ using System.Collections.Generic;
 using System.Linq;
 using Retinues.Model.Characters;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Party.PartyComponents;
+using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.Core;
 using TaleWorlds.ObjectSystem;
 
 namespace Retinues.Model.Factions
 {
-    /// <summary>
-    /// Common wrapper logic for any MBObjectBase that implements IFaction
-    /// (e.g. Clan, Kingdom).
-    /// </summary>
     public abstract class BaseFactionWrapper<TWrapper, TFaction>(TFaction @base)
         : BaseFaction<TWrapper, TFaction>(@base)
         where TWrapper : BaseFactionWrapper<TWrapper, TFaction>
@@ -30,16 +28,63 @@ namespace Retinues.Model.Factions
 #endif
 
         // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
-        //                       Characters                       //
+        //                         Culture                        //
         // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
 
-        /* ━━━━━━━━ Heroes ━━━━━━━━ */
+        public WCulture Culture => WCulture.Get(Base.Culture);
 
-        public override List<WHero> RosterHeroes =>
-            [.. Base.Heroes.ToList().Select(hero => new WHero(hero))];
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+        //                         Heroes                         //
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+
+        public WHero Leader => WHero.Get(Base.Leader);
+
+        public override List<WHero> RosterHeroes => [.. Base.Heroes.Select(WHero.Get)];
+
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+        //                       Characters                       //
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
 
         /* ━━━━━━━━━ Roots ━━━━━━━━ */
 
         public override WCharacter RootBasic => WCharacter.Get(Base.BasicTroop);
+
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+        //                        Territory                       //
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+
+        public IReadOnlyList<Settlement> Settlements => Base.Settlements;
+        public IReadOnlyList<Town> Fiefs => Base.Fiefs;
+
+        /// <summary>
+        /// Convenience flag for "owns at least one fief".
+        /// </summary>
+        public bool HasFiefs => Base.Fiefs != null && Base.Fiefs.Count > 0;
+
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+        //                       War Parties                      //
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+
+        public IReadOnlyList<WarPartyComponent> WarPartyComponents =>
+            Base.WarPartyComponents;
+
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+        //                      Flags & Stats                     //
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+
+        public bool IsBanditFaction => Base.IsBanditFaction;
+        public bool IsMinorFaction => Base.IsMinorFaction;
+        public bool IsKingdomFaction => Base.IsKingdomFaction;
+        public bool IsClan => Base.IsClan;
+        public bool IsOutlaw => Base.IsOutlaw;
+        public bool IsMapFaction => Base.IsMapFaction;
+        public float TotalStrength =>
+#if BL13
+            Base.CurrentTotalStrength;
+#else
+            Base.TotalStrength;
+#endif
+        public bool IsEliminated => Base.IsEliminated;
+        public float Aggressiveness => Base.Aggressiveness;
     }
 }
---
+++ src/Retinues/Model/Settlements/WSettlement.cs
@@ -0,0 +1,58 @@
+using Retinues.Model.Characters;
+using Retinues.Model.Factions;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Settlements;
+
+namespace Retinues.Model.Settlements
+{
+    public class WSettlement(Settlement @base) : WBase<WSettlement, Settlement>(@base)
+    {
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+        //                        Identity                        //
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+
+        public string Name => Base.Name?.ToString() ?? Base.StringId;
+
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+        //                         Culture                        //
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+
+        public WCulture Culture => WCulture.Get(Base.Culture);
+
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+        //                        Ownership                       //
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+
+        public WClan OwnerClan => WClan.Get(Base.OwnerClan);
+        public WHero OwnerHero => WHero.Get(Base.Owner);
+
+        public IFaction MapFaction => Base.MapFaction;
+
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+        //                       Kind / Type                      //
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+
+        public bool IsTown => Base.IsTown;
+        public bool IsCastle => Base.IsCastle;
+        public bool IsVillage => Base.IsVillage;
+        public bool IsHideout => Base.IsHideout;
+        public bool IsFortification => Base.IsFortification;
+
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+        //                       Components                       //
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+
+        public Town Town => Base.Town;
+        public Village Village => Base.Village;
+        public Hideout Hideout => Base.Hideout;
+
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+        //                        Garrison                        //
+        // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+
+        public MobileParty GarrisonParty => Base.Town?.GarrisonParty;
+
+        public bool HasGarrison => GarrisonParty != null;
+    }
+}
